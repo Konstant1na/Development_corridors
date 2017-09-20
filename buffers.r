8f345680352a495b8df46a90fe79d50a90079d6b
Konstant1na <Kspiliopoulou@hotmail.com>
2017-09-20 13:29:24 +0300

    Add files via upload

---
+++ buffers.r
@@ -0,0 +1,11 @@
+
+		##########################
+		######## BUFFERS #########
+		##########################
+
+
+setwd("C:/Users/Konstantina/Desktop/Distance/Comparison/buffer/zonal_euclid")
+		
+file_list <- list.files()
+		
+for ()
